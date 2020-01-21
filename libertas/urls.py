1d9c181ae614df511c871f762e3368401ed6c285
Linus <linus_koester@t-online.de>
2020-01-21 17:02:42 +0100

    moved urls

--- libertas/urls.py
+++ libertas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='libertas/login.html')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/?info=logout'))
 ]
