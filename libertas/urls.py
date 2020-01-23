07965ee237ccf7dc6110088276a1f14c1f5a2241
Linus <linus_koester@t-online.de>
2020-01-23 19:39:22 +0100

    basic sign-up form

--- libertas/urls.py
+++ libertas/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='libertas/login.html')),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/?info=logout'))
+    path('logout/', auth_views.LogoutView.as_view(next_page='/?info=logout')),
+    re_path(r'^signup/$', views.signup, name='signup'),
+    re_path(r'^account_activation_sent/$', views.account_activation_sent,
+            name='account_activation_sent'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            views.activate, name='activate'),
 ]
--- libertas/views.py
+++ libertas/views.py
@@ -1,8 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .forms import SignUpForm
+from .tokens import account_activation_token
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'libertas/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('libertas/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect('account_activation_sent')
+    else:
+        form = SignUpForm()
+    return render(request, 'libertas/signup.html', {'form': form})
+
+
+def account_activation_sent(request):
+    return render(request, 'libertas/account_activation_sent.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+        return redirect('index')
+    else:
+        return render(request, 'libertas/account_activation_invalid.html')
