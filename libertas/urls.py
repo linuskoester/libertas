96a24510a2f32806bb2c0131c1454064fc1c7980
Linus <linus_koester@t-online.de>
2020-01-26 19:26:48 +0100

    better url scheme

--- libertas/urls.py
+++ libertas/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
     path('signout/', auth_views.LogoutView.as_view(next_page='/?info=signout'), name='signout'),
-    re_path(r'^signup/$', views.signup, name='signup'),
-    re_path(r'^account_activation_sent/$', views.account_activation_sent,
-            name='account_activation_sent'),
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path('signup/', views.signup, name='signup'),
+    path('signup/sent', views.account_activation_sent,
+         name='account_activation_sent'),
+    re_path(r'^signup/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             views.activate, name='activate'),
 ]
