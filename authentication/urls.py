9a37823cda5861f6b7934544e007a055b0f97640
Linus <linus_koester@t-online.de>
2020-08-30 12:56:57 +0200

    Hotfix

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     # Registrierung
     path('signup/', views.signup, name='signup'),
-    re_path(r'^signup/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    re_path(r'^signup/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,40})/$',
             views.signup_activate, name='signup_activate'),
     # Passwort zurücksetzen
     path('reset/', views.reset, name='reset'),
-    re_path(r'^reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    re_path(r'^reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,40})/$',
             views.reset_confirm, name='reset_confirm'),
     # Account-Verwaltung
     path('account/', views.account_info, name='account'),
